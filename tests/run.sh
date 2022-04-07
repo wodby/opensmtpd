85b834ad4044c49ca198ffd9647a954907dd2737
Chingis Sandanov <chingis@wodby.com>
2022-04-07 13:25:11 +0800

    Fix tests for newer version

--- tests/run.sh
+++ tests/run.sh
@@ -21,7 +21,7 @@ opensmtpd make check-ready host="${name}" max_try=10
 echo "OK"
 
 echo -n "Checking OpenSMTPD version... "
-opensmtpd smtpd -h 2>&1 | grep -q "OpenSMTPD 6.7.*"
+opensmtpd smtpd -h 2>&1 | grep -q "OpenSMTPD 6.*"
 echo "OK"
 
 echo -n "Validating OpenSMTPD config... "
